0f0924106ce58c6b68e586085cbccea200edbeb6
Maick Souza <maick_a_s@msn.com>
2022-11-15 12:00:50 -0300

    chore: setting axios to fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import { api } from "./utils/api";
+
 function App() {
+  const [loadEducatorsData, setLoadEducatorsData] = useState([]);
+
+  async function loadEducators() {
+    const response = await api.get("educators");
+    setLoadEducatorsData(response.data);
+  }
+
+  useEffect(() => {
+    loadEducators();
+  }, []);
+
   return (
     <div className='bg-black w-full h-screen'>
-      <h1 className='text-white'>Maick Souza</h1>
+      <h1 className='text-white'>
+        <pre>{JSON.stringify(loadEducatorsData, null, 2)}</pre>
+      </h1>
     </div>
   );
 }
