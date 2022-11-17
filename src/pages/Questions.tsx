3e35545f74cb15c1641c04cd28bcbb6ac2b8d8ea
Maick Souza <maick_a_s@msn.com>
2022-11-17 17:13:09 -0300

    chore: adjusting form

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,17 +1,27 @@
+import { useForm } from "react-hook-form";
+
 export function Questions() {
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const watchTypeQuestion = watch("type-question", false);
+
   return (
     <div className='bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 h-screen'>
       <header>CABEÇALHO</header>
+
       <h1>Educador, aqui você pode cadastrar suas questões</h1>
       <label className='block' htmlFor='type-question'>
         Selecione o tipo de questão:
       </label>
       <select
         className='bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200'
-        name='type-question'>
-        <option value='none' selected>
-          Tipo
-        </option>
+        {...register("type-question")}>
+        <option value='none'>Tipo</option>
         <option value='ESSAY'>Dissertativa</option>
         <option value='MULTIPLE_CHOICE'>Múltipla-escolha</option>
       </select>
@@ -34,6 +44,11 @@ export function Questions() {
         Para o dev: aqui precisa fazer uma lógica no front para colocar um campo
         caso seja dissertativa e várias se múltipla escolha
       </span>
+      {watchTypeQuestion === "none" ? <h1>Precisa selecionar</h1> : null}
+      {watchTypeQuestion === "ESSAY" ? <h1>Dissertativa</h1> : null}
+      {watchTypeQuestion === "MULTIPLE_CHOICE" ? (
+        <h1>Múltipla escolha</h1>
+      ) : null}
     </div>
   );
 }
