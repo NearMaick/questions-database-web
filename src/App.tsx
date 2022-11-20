ddee69af434dc97440b27e2ba576b2e5236b1538
Maick Souza <maick_a_s@msn.com>
2022-11-20 13:56:21 -0300

    style: making create question form #2

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { router } from "./router";
 export function App() {
   return (
     <div className='dark h-[110vh] bg-gray-800'>
+      <header className='w-full text-center p-4 font-bold text-5xl text-gray-800 dark:text-gray-200'>
+        CABEÇALHO
+      </header>
       <RouterProvider router={router} />
     </div>
   );
