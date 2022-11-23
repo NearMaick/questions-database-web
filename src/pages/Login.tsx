33fe4676c6a3226f2947a49dd339e9ac25ecc778
Maick Souza <maick_a_s@msn.com>
2022-11-23 12:07:46 -0300

    chore: fetch token from rest api

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,55 @@
+import { useForm } from "react-hook-form";
+import { api } from "../utils/api";
+
+interface FormValues {
+  email: string;
+  password: string;
+}
+
 export function Login() {
+  const { register, watch, reset, handleSubmit } = useForm<FormValues>();
+
+  async function handleAuthenticate(data: FormValues) {
+    const response = await api.post("/educators/auth", {
+      email: data.email,
+      password: data.password,
+    });
+    console.log(response.data);
+  }
+
   return (
-    <div className='mx-auto flex items-center w-[768px] max-w-[768px] h-[80vh] '>
-      <div className='px-2 my-4 mx-auto flex flex-col items-center justify-center bg-gray-200 border dark:bg-gray-800 text-gray-800 dark:text-gray-200 dark:border-white border-black h-[60%] w-[60%] rounded-md'>
-        <label className='inline-block p-2' htmlFor='subject'>
-          E-mail
-        </label>
-        <input
-          className='bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded-md p-2 mb-2 w-3/5 h-6'
-          type='text'
-          required
-        />
-        <label className='inline-block p-2' htmlFor='subject'>
-          Senha
-        </label>
-        <input
-          className='bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded-md p-2 w-3/5 h-6'
-          type='text'
-          required
-        />
+    <form onSubmit={handleSubmit(handleAuthenticate)}>
+      <div className='mx-auto flex items-center w-[768px] max-w-[768px] h-[80vh] '>
+        <div className='px-2 my-4 mx-auto flex flex-col items-center justify-center bg-gray-200 border dark:bg-gray-800 text-gray-800 dark:text-gray-200 dark:border-white border-black h-[60%] w-[60%] rounded-md'>
+          <label className='inline-block p-2' htmlFor='email'>
+            E-mail
+          </label>
+          <input
+            className='bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded-md p-2 mb-2 w-3/5 h-6'
+            type='email'
+            {...register("email")}
+            required
+          />
+          <label className='inline-block p-2' htmlFor='password'>
+            Senha
+          </label>
+          <input
+            className='bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded-md p-2 w-3/5 h-6'
+            type='password'
+            {...register("password")}
+            required
+          />
+
+          <div className='w-full flex justify-center'>
+            <button
+              type='submit'
+              className='my-4 py-4 px-10 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded-md text-lg dark:hover:bg-gray-600 transition'>
+              Enviar
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
 
