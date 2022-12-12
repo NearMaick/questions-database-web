e230b945b6d3456ee2236af60e2fd4227001ec83
Maick Souza <maick_a_s@msn.com>
2022-12-12 11:24:56 -0300

    feat: save and printing list questions

--- src/pages/QuestionsGenerate.tsx
+++ src/pages/QuestionsGenerate.tsx
@@ -1,8 +1,14 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { api } from "../utils/api";
 
+interface IQuestionsProps {
+  id: string;
+  description: string;
+  answer: string[];
+}
+
 export function QuestionsGenerate() {
-  const [listQuestions, setListQuestions] = useState<any[]>([]);
+  const [listQuestions, setListQuestions] = useState<IQuestionsProps[]>([]);
 
   async function loadListedQuestionsData() {
     const storedItemsAsJSON = localStorage.getItem(
@@ -26,11 +32,43 @@ export function QuestionsGenerate() {
     loadListedQuestionsData();
   }, []);
 
+  function handleSaveListQuestion() {
+    const storedItemsAsJSON = localStorage.getItem(
+      "@QuestionsDatabase-1.0.0:questions-list"
+    );
+    const { questions } = JSON.parse(storedItemsAsJSON!);
+
+    console.log(questions);
+
+    window.print();
+  }
+
   return (
     <div className='text-gray-100 w-full'>
-      <div className='grid grid-cols-2 gap-2 bg-gray-800'>
-        <h1>Imprimir a lista</h1>
-        <pre>{JSON.stringify(listQuestions, null, 2)}</pre>
+      <div className='bg-gray-800'>
+        <button onClick={handleSaveListQuestion}>Imprimir a lista</button>
+        {listQuestions.map((question) => (
+          <Fragment key={question.id}>
+            {question.answer.length === 1 ? (
+              <>
+                <li>{question.description}</li>
+                <p>_____________________________________________________</p>
+              </>
+            ) : (
+              <>
+                <p>{question.description}</p>
+                {question.answer.map((answer) => (
+                  <Fragment key={answer}>
+                    <input type='checkbox' disabled />
+                    <label>{answer}</label>
+                    <br />
+                  </Fragment>
+                ))}
+              </>
+            )}
+            <br />
+          </Fragment>
+        ))}
       </div>
     </div>
   );
